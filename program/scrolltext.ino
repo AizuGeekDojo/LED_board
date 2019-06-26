df2ca53cab0bb255bc49e90b609b110d9e4b4a03
Fumito Haga <s1240056@gmail.com>
2019-06-26 11:46:47 +0900

    Update scrolltext.ino
    fix invalid pin assign

--- program/scrolltext.ino
+++ program/scrolltext.ino
@@ -20,8 +20,8 @@
 #define CLK  8   // USE THIS ON ARDUINO UNO, ADAFRUIT METRO M0, etc.
 //#define CLK A4 // USE THIS ON METRO M4 (not M0)
 //#define CLK 11 // USE THIS ON ARDUINO MEGA
-#define OE   9
-#define LAT 10
+#define OE  10
+#define LAT  9
 #define A   A0
 #define B   A1
 #define C   A2
